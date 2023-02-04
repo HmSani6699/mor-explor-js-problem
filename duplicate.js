81b29978719c2600f4b0f159932d223c9112d556
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-04 20:58:10 +0600

    array duplicate item remove part

--- duplicate.js
+++ duplicate.js
@@ -25,4 +25,22 @@ function duplicate(array) {
 }
 let number_1 = [1, 2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7, 8, 4, 5, 3];
 let output_1 = duplicate(number_1);
-console.log(output_1);
+// console.log(output_1);
+
+
+
+/* -----Remove Duplicate item in Array------*/
+
+let num = [1, 2, 3, 4, 5, 2, 4, 5, 1, 2, 3, 4, 5, 1, 1];
+let duplicateNumber = [];
+let main = []
+for (let i = 0; i < num.length; i++) {
+    let element = num[i];
+    if (main.includes(element) === false) {
+        main.push(element)
+    }
+    else {
+        duplicateNumber.push(element)
+    }
+}
+console.log(main, duplicateNumber)
