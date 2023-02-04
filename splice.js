bcb66095208ac46d4c64ad64c78464d35c77f63f
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-04 19:35:17 +0600

    ad to array splice part

--- splice.js
+++ splice.js
@@ -1,5 +1,11 @@
 let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let splice = number.splice(3, 3, 30, 30, 30);
-console.log(splice);
-console.log(number);
+// console.log(splice);
+// console.log(number);
+
+/* -----------Array splice---------*/
+let friendsName = ['babu', 'sakib', 'rakib', 'hasib', 'noman'];
+let arraySplice = friendsName.splice(2, 1, '66666', '999');
+console.log(friendsName)
+console.log(arraySplice)
