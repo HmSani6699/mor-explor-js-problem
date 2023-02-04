0b88f31b629b7c8d80f15a23fd97548f840574b6
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-04 19:29:12 +0600

    ad to array slice part

--- slice.js
+++ slice.js
@@ -1,5 +1,10 @@
 let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(number);
+// console.log(number);
 
 let slice = number.slice(3, 7);
-console.log(slice)
+// console.log(slice)
+
+/* -----Array slice------- */
+let friendsName = ['babu', 'sakib', 'rakib', 'hasib', 'noman', 'josim'];
+let arraySlice = friendsName.slice(3, 5);
+// console.log(arraySlice)
