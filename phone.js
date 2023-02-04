aec0dfadc0fd747374d5ce7a5eb93f1f8bf03fc9
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-04 22:03:38 +0600

    get lowest phone

---
+++ phone.js
@@ -0,0 +1,26 @@
+let allPhone = [
+    { name: 'nokia', price: 3000, camera: 300 },
+    { name: 'mi', price: 2500, camera: 100 },
+    { name: 'iphone', price: 10000, camera: 500 },
+    { name: 'realme', price: 1000, camera: 700 },
+    { name: 'appo', price: 5000, camera: 400 },
+];
+
+let lowestPhone = allPhone[0]
+for (let i = 0; i < allPhone.length; i++) {
+    let element = allPhone[i];
+    if (element.price < lowestPhone.price) {
+        lowestPhone = element
+    }
+}
+// console.log(lowestPhone)
+
+let friends = ['sani mia Hm', 'babu Molla', 'noman', 'sahabodin'];
+let bigName = '';
+for (let i = 0; i < friends.length; i++) {
+    let element = friends[i];
+    if (element.length > bigName.length) {
+        bigName = element
+    }
+}
+console.log(bigName)
