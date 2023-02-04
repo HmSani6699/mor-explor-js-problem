97b958293d7dc8d43117a841b13e804c9c81c859
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-04 22:16:39 +0600

    ad to card part

--- card.js
+++ card.js
@@ -16,4 +16,26 @@ function sum(shoppingCard) {
 }
 
 let result = sum(shoppingCard);
-console.log(result);
+// console.log(result);
+
+
+
+let shoppingCard_1 = [
+    { name: 'apple', price: 20, quantity: 2 },
+    { name: 'orange', price: 10, quantity: 4 },
+    { name: 'banana', price: 5, quantity: 3 },
+    { name: 'mango', price: 30, quantity: 1 },
+    { name: 'grep', price: 15, quantity: 5 }
+];
+
+function total() {
+    let sum_1 = 0;
+    for (let i = 0; i < shoppingCard_1.length; i++) {
+        let element = shoppingCard_1[i];
+        let multiply = element.price * element.quantity;
+        sum_1 += multiply;
+    }
+    return sum_1
+}
+let output = total(shoppingCard_1)
+console.log(output)
--- phone.js
+++ phone.js
@@ -23,4 +23,4 @@ for (let i = 0; i < friends.length; i++) {
         bigName = element
     }
 }
-console.log(bigName)
+// console.log(bigName)
